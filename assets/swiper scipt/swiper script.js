6602ec219a22b1979eabdf68c7b43a695960096f
Abdullah801953 <ak786shariq@gmail.com>
2024-12-16 22:16:07 +0530

    mahta commit

--- assets/swiper scipt/swiper script.js
+++ assets/swiper scipt/swiper script.js
@@ -1,13 +1,27 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 6,
-    spaceBetween: 70,
-    autoplay: {
-      delay: 3000, // Time between slides (in milliseconds)
-      disableOnInteraction: false, // Continue autoplay after user interaction
+  slidesPerView: 6,
+  spaceBetween: 70,
+  autoplay: {
+    delay: 3000, // Time between slides (in milliseconds)
+    disableOnInteraction: false, // Continue autoplay after user interaction
+  },
+  breakpoints: {
+    // When window width is >= 768px
+    768: {
+      slidesPerView: 5, // Show 4 slides
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      enabled:false,
+    // When window width is >= 480px
+    480: {
+      slidesPerView: 3, // Show 2 slides
     },
-  });
+    // When window width is < 480px
+    0: {
+      slidesPerView: 1, // Show 1 slide
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    enabled: false,
+  },
+});
